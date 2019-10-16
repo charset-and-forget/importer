aabbab0e28c17b4698b8c8c516a28633a773c459
Andrey Gubarev <mylokin@me.com>
2019-10-16 17:37:11 +0300

    init mongodb connection

--- src/script.py
+++ src/script.py
@@ -1,7 +1,13 @@
 import argparse
+import os
+
+import pymongo
+MONGODB = pymongo.MongoClient(os.getenv('MONGODB'))
 
 
 def main():
+    MONGODB['importer'].command('ping')
+
     parser = argparse.ArgumentParser(description='Custom Importer')
     args = parser.parse_args()
 
