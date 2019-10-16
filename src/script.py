426f3b58981c1e9cad69f4daf5542e2204914f1c
Andrey Gubarev <mylokin@me.com>
2019-10-16 19:13:38 +0300

    update docker-compose

--- src/script.py
+++ src/script.py
@@ -2,11 +2,14 @@ import argparse
 import os
 
 import pymongo
+
+import importer
 MONGODB = pymongo.MongoClient(os.getenv('MONGODB'))
 
 
 def main():
     MONGODB['importer'].command('ping')
+    os.listdir(os.getenv('BASEPATH_DATA'))
 
     parser = argparse.ArgumentParser(description='Custom Importer')
     args = parser.parse_args()
