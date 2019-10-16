211b9f0ece626b277563a61f553299eac06901c3
Andrey Gubarev <mylokin@me.com>
2019-10-16 19:17:23 +0300

    remove argparse

--- src/script.py
+++ src/script.py
@@ -1,4 +1,3 @@
-import argparse
 import os
 
 import pymongo
@@ -10,9 +9,6 @@ MONGODB = pymongo.MongoClient(os.getenv('MONGODB'))
 def main():
     MONGODB['importer'].command('ping')
     os.listdir(os.getenv('BASEPATH_DATA'))
-
-    parser = argparse.ArgumentParser(description='Custom Importer')
-    args = parser.parse_args()
 
 
 if __name__ == '__main__':
