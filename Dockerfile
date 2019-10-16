387ee2e606a8b66e9d9714d56e38b3755a8ecc6e
Andrey Gubarev <mylokin@me.com>
2019-10-16 17:08:16 +0300

    add argparse

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ WORKDIR /usr/src/app
 
 COPY src/* ./
 RUN pip install --no-cache-dir -r requirements.txt
-RUN pycodestyle --statistics -qq ./
+RUN pycodestyle ./
 
 ENTRYPOINT python script.py
---
+++ src/script.py
@@ -0,0 +1,10 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Custom Importer')
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
